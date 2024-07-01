71a826d8b85aeb91c521ce076842a4493d9bdb23
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-07-01 19:06:41 +0600

    add validation for form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,51 +6,70 @@ import eyeOpenedImg from '/imgs/eye-opened.svg'
 import eyeClosedImg from '/imgs/eye-closed.svg'
 import background from '/imgs/background.svg'
 
+import { basicSchema } from "../schemas"
 
 export default function Register() {
 
   const userRef = useRef()
   const errorRef = useRef()
-  
+
   const [errorMessage, setErrorMessage] = useState('')
   const [succes, setSucces] = useState(false)
 
+  const onSubmit = (values, actions)=>{
+    console.log(values)
+  }
+
 
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handlePasswordShow = () => setShowPassword(!showPassword);
+  const handleConfirmPasswordShow = () => setShowConfirmPassword(!showConfirmPassword);
 
-  const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-
-  const {values, handleBlur, handleChange} = useFormik({
+  const {values, errors, touched, handleBlur, handleChange, handleSubmit, isSubmitting} = useFormik({
     initialValues: {
-      login: "",
-      password: "",
+      email: '',
+      login: '',
+      password: '',
+      confirmPassword: '',
     },
+    validationSchema: basicSchema,
+    onSubmit,
   });
-
-  useEffect(()=>{
-    // useRef.current.focus()
-  },[])
 
   return (
     <>
       <img className="background__img" src={background} alt="background" />
       <div className="form__section">
         <h2>Вэлком бэк!</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
+          {errors.email && touched.email && <p className="error">{errors.email}</p>}
+          <input
+            type='email'
+            className={errors.email && touched.email ? "input-error" : ""}
+            placeholder='Введи адрес почты'
+            name='email'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.login && touched.login && <p className="error">{errors.login}</p>}
           <input
             type="text"
+            className={errors.login && touched.login ? "input-error" : ""}
             id="login"
-            placeholder="Введи туда-сюда логин"
+            placeholder="Придумай логин"
             name="login"
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.login}
           />
           <div className="input__container">
+            {errors.password && touched.password && <p className="error">{errors.password}</p>}
             <input
               type={showPassword ? "text" : "password"}
+              className={errors.password && touched.password ? "input-error" : ""}
               placeholder="Пароль (тоже введи)"
               name="password"
               id="password"
@@ -60,11 +79,30 @@ export default function Register() {
             />
             <img
               src={showPassword ? eyeClosedImg : eyeOpenedImg}
-              alt="eye opened"
+              alt="eyeImg"
               onClick={handlePasswordShow}
             />
           </div>
-          <button className="login__btn">Далее</button>
+          <div className="input__container">
+            {errors.confirmPassword && touched.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
+              placeholder='Повтори пароль'
+              name='confirmPassword'
+              id="confirmPassword"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.confirmPassword}
+            />
+            <img
+              src={showConfirmPassword ? eyeClosedImg : eyeOpenedImg}
+              alt="eyeImg"
+              onClick={handleConfirmPasswordShow}
+            />
+          </div>
+          <button type="submit" className="login__btn">Далее</button>
+          {isSubmitting ? (<h1>submitin</h1>) : ''}
         </form>
       </div>
     </>
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,12 +1,23 @@
 import * as yup from "yup"
 
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{5,24}$/;
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 
 
-const basicSchema = yup.object().shape({
+export const basicSchema = yup.object().shape({
     email: yup.string().email("Please eneter valid email").required("Requiered"),
     age: yup.number().positive().integer().required("Requiered"),
-    password: yup.string().min(5).matches(PWD_REGEX, {message:" Please create stronger password"}).required("Requiered"),
+    login: yup.string().min(5).matches(USER_REGEX, {message: 'Только латинские буквы разрешены'}),
+    // password: yup.string().min(5).matches(PWD_REGEX, {message:" Please create stronger password"}).required("Requiered"),
+    password: yup
+    .string()
+    .min(5, 'От 8 до 15 символов')
+    .max(15, 'От 8 до 15 символов')
+    .matches(/[a-z]/, 'Строчные и прописные буквы')
+    .matches(/[A-Z]/, 'Строчные и прописные буквы')
+    .matches(/\d/, 'Минимум 1 цифра')
+    .matches(/[^a-zA-Z0-9]/, 'Минимум 1 спецсимвол (!, ", #, $...)')
+    .required('Требуется пароль'),
     confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Password must match").required("Required")
 })
 
